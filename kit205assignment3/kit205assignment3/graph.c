0e94bd848d9e090654d1c860d896a147a537043c
Tarnsky <ted.krimmer@gmail.com>
2021-10-14 13:49:48 +1100

    comments

--- kit205assignment3/kit205assignment3/graph.c
+++ kit205assignment3/kit205assignment3/graph.c
@@ -15,12 +15,18 @@ void init_graph(Graph* self)
 	}
 
 }
+/*
+* Input edges data from input.txt for Graph
+* @param Graph
+*/
 void add_edge(Graph* self) {
+//Scan data from input.txt
 	for (int i = 0; i < self->V; i++) {
 		int temp;
 		scanf("%d", &temp);
 
 		for (int j = 0; j < temp; j++) {
+//Creates new node and scans in to_vertex and weight from input.txt
 			Edge new_node;
 			scanf("%d,%d", &new_node.to_vertex, &new_node.weight);
 			//printf("\n At city %d \n", i);
@@ -29,7 +35,7 @@ void add_edge(Graph* self) {
 			int current_node = new_node.to_vertex;
 			insert_at_front(&self->edges[i], new_node);
 			int in_degree = malloc(sizeof(self->V));
-			//create link between Cities
+//Create link between Cities because the graph is undirected so edges go both ways.
 			int from_node = i;
 			//printf("\n from node %d \n", from_node);
 			Edge new_node_link;
@@ -37,12 +43,16 @@ void add_edge(Graph* self) {
 			//printf("\n link node link to %d \n", i);
 			new_node_link.weight = new_node.weight;
 			//printf("\n link node link weight to %d", new_node_link.weight);
-
+//insert data to head of list
 			insert_at_front(&self->edges[current_node], new_node_link);
 		}
 	}
 }
-
+/*
+* inserts node data to the head of the EdgeList
+* @param EdgeList
+* @param node
+*/
 void insert_at_front(EdgeList* self, Edge data) {
 	EdgeNodePtr new_node = malloc(sizeof * new_node);
 	//printf("\n Adding edge to %d \n", data.to_vertex);
@@ -50,13 +60,16 @@ void insert_at_front(EdgeList* self, Edge data) {
 	new_node->next = self->head;
 	self->head = new_node;
 }
-//6
+/*
+* Print Graph showing the city then listing all edges and cost(weight)
+* @param Graph
+*/
 void print_graph(Graph* self) {
 	for (int i = 0; i < self->V; i++)
 	{
 		printf("City %d \n", i);
 		EdgeNodePtr current = self->edges[i].head;
-		//7
+
 		while (current != NULL) {
 			printf("\n connects to city %d with a ticket cost of %d \n", current->edge.to_vertex, current->edge.weight);
 			current = current->next;
@@ -64,6 +77,10 @@ void print_graph(Graph* self) {
 		printf("\n");
 	}
 }
+/*
+* Calculates the "in degrees" for a graph. Loops through though graph adding 1 for every edge that points to a City
+* @param Graph
+*/
 void in_degree(Graph* self) {
 	int* in_degree = malloc(self->V);
 	for (int i = 0; i < self->V; i++) {
@@ -85,40 +102,70 @@ void in_degree(Graph* self) {
 	}
 }
 
+/*
+* Free all used by Graph
+* @param Graph
+*/
+void destroy_graph(Graph* self) {
+	for (int i = 0; i < self->V; i++) {
+		EdgeNodePtr current = self->edges[i].head;
+
+		while (current != NULL) {
+			EdgeNodePtr to_free = current;
+			current = current->next;
+			free(to_free);
+		}
+	}
+
+	free(self->edges);
+}
+/*
+* returns a MST from Graph using prims algorithm 
+* @param Graph
+* @return newly-created Graph(F) to represent the MST
+*/
 Graph prims_mst(Graph* self) {
 	int* min_cost_to_v = malloc(self->V);
 	int* visited = malloc(self->V);
 	int cost = 0;
+//Inisitlises all min_cost and visted
 	for (int i = 0; i < self->V; i++)
 	{
 		min_cost_to_v[i] = 100;
 		visited[i] = 666;
 	}
+//Create new Graph "F" which will be the MST
 	Graph F;
+//Set the number of cities equal to the number of cities for param Graph
 	F.V = self->V;
+//Inistlise empty edges for graph
 	init_graph(&F);
 	int* Q = malloc(self->V);
+//Set the root city to visted 
 	visited[0] = 1;
+//Loop though untill all cities have been checked for connections 
 	for (int i = 0; i < self->V; i++)
 	{
+//Check each city to see if it has been visted
 		for (int i = 0; i < self->V; i++)
 		{
+//Check to see if the city has been visited. If so check edges to see if a new connection can be made
 			if (visited[i] == 1)
 			{
 				EdgeNodePtr current = self->edges[i].head;
 				while (current != NULL) {
 					if (visited[current->edge.to_vertex] == 666) {
-						//set the min cost to the weigh of the edge 
+//Set the min cost to the weigh of the edge 
 						min_cost_to_v[i] = current->edge.weight;
-						//add the node to graph F
+//Add the node to graph F
 						Edge new_node;
 						//printf("\n At city %d \n", i);
 						new_node.weight = current->edge.weight;
 						new_node.to_vertex = current->edge.to_vertex;
-						//set node to flag as visited 
+//Set node to flag as visited 
 						visited[current->edge.to_vertex] = 1;
 						insert_at_front(&F.edges[i], new_node);
-						//calculate total cost of MST
+//Calculate total cost of MST
 						cost = cost + min_cost_to_v[i];
 					}
 					current = current->next;
--- kit205assignment3/kit205assignment3/graph.h
+++ kit205assignment3/kit205assignment3/graph.h
@@ -1,26 +1,75 @@
+
+/*
+* Represents an edge in a graph
+*/
 typedef struct edge {
     int to_vertex;
     int weight;
 } Edge;
 
+/*
+* Represents a linked list node containing a graph
+*/
 typedef struct edgeNode {
     Edge edge;
     struct edgeNode* next;
 } *EdgeNodePtr;
 
+/*
+* Represents a linked list of edges 
+*/
 typedef struct edgeList {
     EdgeNodePtr head;
 } EdgeList;
 
+/*
+* Represents an graph with vertices and edges
+*/
 typedef struct graph {
     int V;
     EdgeList* edges;
 } Graph;
 
+/*
+* Initialize Graph with empty edges
+* @param Graph
+*/
 void init_graph(Graph* self);
+
+/*
+* Input edges data from input.txt for Graph
+* @param Graph
+*/
 void add_edge(Graph* self);
+
+/*
+* inserts node data to the head of the EdgeList
+* @param EdgeList
+* @param node
+*/
 void insert_at_front(EdgeList* self, Edge data);
+
+/*
+* Print Graph showing the city then listing all edges and cost(weight)
+* @param Graph
+*/
 void print_graph(Graph* self);
+
+/*
+* Calculates the "in degrees" for a graph. Loops through though graph adding 1 for every edge that points to a City
+* @param Graph
+*/
 void in_degree(Graph* self);
-//void destroy_graph(Graph* G);
+
+/*
+* Free all used by Graph
+* @param Graph
+*/
+void destroy_graph(Graph* G);
+
+/*
+* returns a MST from Graph using prims algorithm
+* @param Graph
+* @return newly-created Graph(F) to represent the MST
+*/
 Graph prims_mst(Graph* self);
